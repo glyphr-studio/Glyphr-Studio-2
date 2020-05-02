181ce03ee676785c11dabba2b578433965aea988
Matt LaGrandeur <mattlag@users.noreply.github.com>
2020-05-01 23:35:33 -0700

    Ugh, just plz ES6 :apple:

---
+++ dev/common/colors.test.mjs
@@ -0,0 +1,21 @@
+import {parseColorString, shiftColor, getColorFromRGBA, transparencyToAlpha} from './colors.js';
+// jest.moc('./colors.js');
+
+describe('Colors', () => {
+  it('parseColorString', () => {
+    expect(parseColorString('rgb(123,45,67')).toBe({r: 123, g: 45, b: 67, a: 1});
+  });
+
+  it('shiftColor', () => {
+    expect(shiftColor('rgb(123,45,67)', 0.5, true)).toBe('rgb(189,150,161)');
+  });
+
+  it('getColorFromRGBA', () => {
+    /* eslint-disable new-cap */
+    expect(getColorFromRGBA('rgb(123,45,67)', 0.2)).toBe('rgb(229,213,217)');
+  });
+
+  it('transparencyToAlpha', () => {
+    expect(transparencyToAlpha(45)).toBe(0.55);
+  });
+});
