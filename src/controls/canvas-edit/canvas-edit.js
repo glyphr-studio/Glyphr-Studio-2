fb1565e055cb8cc5b96390c9c8296768974efd6c
Matt LaGrandeur <mattlag@gmail.com>
2022-08-19 00:19:13 -0700

    View getters/setters :movie_camera:

--- src/controls/canvas-edit/canvas-edit.js
+++ src/controls/canvas-edit/canvas-edit.js
@@ -98,7 +98,6 @@ export default class CanvasEdit extends HTMLElement {
         this.width = newValue;
         this.redraw();
       break;
-
     }
 
     if (attributeName === 'glyphs') {
@@ -114,132 +113,60 @@ export default class CanvasEdit extends HTMLElement {
     log('CanvasEdit.redraw', 'start');
     let editor = getCurrentProjectEditor();
     let glyph = editor.selectedGlyph;
-    let settings = getCurrentProject().projectSettings;
-    let gutterSize = 20;
-    let contentWidth = this.width - (2*gutterSize);
-    let contentHeight = this.height - (2*gutterSize);
-    let upm = settings.upm;
-    let ascent = settings.ascent;
-    let zoom = Math.min(contentWidth, contentHeight) / upm;
-    let glyphWidth = glyph.advanceWidth;
 
-    let view = {
-      dx: gutterSize + ((contentWidth - (zoom * glyphWidth))/2),
-      dy: gutterSize + (zoom * (ascent)),
-      dz: zoom,
-    };
+    if(!editor.viewExists(editor.selectedGlyphID)){
+      let settings = getCurrentProject().projectSettings;
+      let gutterSize = 20;
+      let contentWidth = this.width - (2*gutterSize);
+      let contentHeight = this.height - (2*gutterSize);
+      let upm = settings.upm;
+      let ascent = settings.ascent;
+      let zoom = Math.min(contentWidth, contentHeight) / upm;
+      let glyphWidth = glyph.advanceWidth;
+
+      editor.view = {
+        dx: gutterSize + ((contentWidth - (zoom * glyphWidth))/2),
+        dy: gutterSize + (zoom * (ascent)),
+        dz: zoom,
+      };
+    }
 
     this.ctx.clearRect(0, 0, this.width, this.height);
     this.ctx.fillStyle = accentColors.purple.l60;
-    this.ctx.fillRect(view.dx, 0, 1, 1000);
-    this.ctx.fillRect(0, view.dy, 1000, 1);
+    this.ctx.fillRect(editor.view.dx, 0, 1, 1000);
+    this.ctx.fillRect(0, editor.view.dy, 1000, 1);
 
     let glyphHex = glyphToHex(this.glyphs.charAt(0));
 
     let sg = getCurrentProject().getGlyph(glyphHex);
     log(sg);
-    sg.drawGlyph(this.ctx, view);
+    sg.drawGlyph(this.ctx, editor.view);
     log('CanvasEdit.redraw', 'end');
   }
 }
-
-
-
-// -------------------
-// VIEW
-// -------------------
-
-function setView(oa) {
-	const sc =
-	  editor.nav.page === 'kerning'
-		? getSelectedKernID()
-		: getSelectedWorkItemID();
-	const v = _UI.views || {};
-
-	// Ensure there are at least defaults
-	if (!isVal(v[sc])) {
-	  v[sc] = getView('setView');
-	}
-
-	// Check for which to set
-	if (isVal(oa.dx)) v[sc].dx = oa.dx;
-	if (isVal(oa.dy)) v[sc].dy = oa.dy;
-	if (isVal(oa.dz)) v[sc].dz = oa.dz;
-}
-
-function getView(calledBy) {
-	// log('getView', 'start');
-	// log('calledBy: ' + calledBy);
-
-	const onkern = editor.nav.page === 'kerning';
-	const sc = onkern ? getSelectedKernID() : getSelectedWorkItemID();
-	const v = _UI.views || {};
-	let re;
-
-	if (isVal(v[sc])) {
-	  re = clone(v[sc]);
-	} else {
-	  re = onkern ? clone(_UI.defaultKernView) : clone(_UI.defaultView);
-	}
-
-	// log('returning ' + json(re));
-	// log('getView', 'end');
-
-	return re;
-}
-
-function getDefaultView() {}
-
-function viewZoom(zfactor, center) {
-	const v = getView('viewZoom');
-	const mx = _UI.eventhandlers.mousex;
-	const my = _UI.eventhandlers.mousey;
-
-	setView({
-	  dz: round((v.dz *= zfactor), 2),
-	  dx: center ? v.dx : mx - (mx - v.dx) * zfactor,
-	  dy: center ? v.dy : my - (my - v.dy) * zfactor,
-	});
-
-	redraw({ calledBy: 'viewZoom', redrawPanels: false });
-}
-
-function setViewZoom(zoom) {
-	zoom /= 100;
-	const v = getView('setViewZoom');
-
-	setView({
-	  dz: round(zoom, 2),
-	  dx: v.dx,
-	  dy: v.dy,
-	});
-
-	redraw({ calledBy: 'setViewZoom', redrawPanels: false });
-}
-
 
 
 // --------------------------------------------------------------------------
 // Convert between Saved values and Canvas values
 // --------------------------------------------------------------------------
 // convert stored x-y point to canvas x-y
-export function sXcX(sx, view = getView('sXcX')) {
+export function sXcX(sx, view = getCurrentProjectEditor().view) {
 	let canvasx = view.dx;
 	canvasx += sx * view.dz;
 	return canvasx || view.dx;
 }
 
-export function sYcY(sy, view = getView('sYcY')) {
+export function sYcY(sy, view = getCurrentProjectEditor().view) {
 	let canvasy = view.dy;
 	canvasy -= sy * view.dz;
 	return canvasy || view.dy;
 }
 
 // convert canvas x-y inputs to saved shape x-y
-export function cXsX(cx, view = getView('cXsX')) {
+export function cXsX(cx, view = getCurrentProjectEditor().view) {
 	return (cx - view.dx) / view.dz;
 }
 
-export function cYsY(cy, view = getView('cYsY')) {
+export function cYsY(cy, view = getCurrentProjectEditor().view) {
 	return (view.dy - cy) / view.dz;
 }
