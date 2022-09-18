0dffbc9779001f032a2b225cf099e4892f7d930d
Matt LaGrandeur <mattlag@gmail.com>
2022-09-18 11:05:43 -0700

    tyop

--- src/edit_canvas/tools/tools.js
+++ src/edit_canvas/tools/tools.js
@@ -474,7 +474,7 @@ export function makeToolButtonSVG(oa) {
 		<svg
 			version="1.1"
 			xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
-			x="0px" y="0px" width="20px" height="30px" viewBox="0 0 20 20"
+			x="0px" y="0px" width="20px" height="20px" viewBox="0 0 20 20"
 		>
 			${innerHTML}
 		</svg>
