d55196ad1532ec9fa3c19825d09e810fa1560810
Matt LaGrandeur <mattlag@gmail.com>
2023-10-03 10:30:48 -0700

    TESTS Coord

--- src/common/functions.test.js
+++ src/common/functions.test.js
@@ -1,8 +1,8 @@
 import { describe, expect, it } from 'vitest';
-import { clone, json } from '../functions.js';
+import { clone, json, xyPointsAreClose } from '../functions.js';
 
-describe('clone', () => {
-	it('should create a deep clone of an object', () => {
+describe('Common Functions', () => {
+	it('clone', () => {
 		const originalObject = { prop1: 'value1', prop2: { subProp1: 'subValue1' } };
 		const clonedObject = clone(originalObject);
 
@@ -10,13 +10,17 @@ describe('clone', () => {
 		expect(clonedObject).not.toBe(originalObject);
 		expect(clonedObject.prop2).not.toBe(originalObject.prop2);
 	});
-});
 
-describe('json', () => {
-	it('should stringify an object', () => {
+	it('json', () => {
 		const testObject = { prop1: 'value1', prop2: 'value2' };
 		const jsonString = json(testObject);
 
 		expect(jsonString.replace(/\s+/g, '')).toBe('{"prop1":"value1","prop2":"value2"}');
+	});
+
+	it('xyPointsAreClose', () => {
+		const c1 = { x: 100, y: 100 };
+		const c2 = { x: 99.1, y: 100.9 };
+		expect(xyPointsAreClose(c1, c2)).toBeTruthy();
 	});
 });
