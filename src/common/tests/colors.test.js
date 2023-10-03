4fba16278dec4036522624da797877006e08e7e6
Matt LaGrandeur <mattlag@gmail.com>
2023-10-03 14:23:47 -0700

    TESTS Common Colors

--- src/common/tests/colors.test.js
+++ src/common/tests/colors.test.js
@@ -1,9 +1,19 @@
 import { describe, expect, it } from 'vitest';
-import { getColorFromRGBA, parseColorString, shiftColor, transparencyToAlpha } from '../colors.js';
+import {
+	getColorFromRGBA,
+	makeRandomSaturatedColor,
+	parseColorString,
+	rgbToHex,
+	shiftColor,
+	transparencyToAlpha,
+} from '../colors.js';
 
 describe('Colors', () => {
 	it('parseColorString', () => {
 		expect(parseColorString('rgb(123,45,67')).toEqual({ r: 123, g: 45, b: 67, a: 1 });
+	});
+	it('rgbToHex', () => {
+		expect(rgbToHex('rgb(123,45,67')).toEqual('#7B2D43');
 	});
 
 	it('shiftColor', () => {
@@ -11,11 +21,17 @@ describe('Colors', () => {
 	});
 
 	it('getColorFromRGBA', () => {
-		/* eslint-disable new-cap */
 		expect(getColorFromRGBA('rgb(123,45,67)', 0.2)).toBe('rgb(229,213,217)');
 	});
 
 	it('transparencyToAlpha', () => {
 		expect(transparencyToAlpha(45)).toBe(0.55);
+	});
+
+	it('makeRandomSaturatedColor', () => {
+		let rsc = makeRandomSaturatedColor();
+		expect(rsc.startsWith('rgb(')).toBeTruthy();
+		expect(rsc.indexOf('255') > -1).toBeTruthy();
+		expect(rsc.indexOf('0') > -1).toBeTruthy();
 	});
 });
