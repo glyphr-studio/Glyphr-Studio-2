071b2bb62abbf723108de85356676ce14eb6ae18
Matt LaGrandeur <mattlag@gmail.com>
2023-11-08 16:46:16 -0800

    Basic Panose Helper

---
+++ src/lib/panose.js
@@ -0,0 +1,318 @@
+export const panoseData = [
+	{
+		digit: 0,
+		name: 'Any'
+	},
+	{
+		digit: 1,
+		name: 'No fit'
+	},
+	{
+		digit: 2,
+		name: 'Latin Text',
+		values: [
+			{
+				digit: 0,
+				name: 'Any'
+			},
+			{
+				digit: 1,
+				name: 'No fit'
+			},
+			{
+				digit: 2,
+				name: 'Serif Style',
+				values: [
+
+				]
+			},
+			{
+				digit: 3,
+				name: 'Weight',
+				values: [
+
+				]
+			},
+			{
+				digit: 4,
+				name: 'Proportion',
+				values: [
+
+				]
+			},
+			{
+				digit: 5,
+				name: 'Contrast',
+				values: [
+
+				]
+			},
+			{
+				digit: 6,
+				name: 'Stroke Variation',
+				values: [
+
+				]
+			},
+			{
+				digit: 7,
+				name: 'Arm Style',
+				values: [
+
+				]
+			},
+			{
+				digit: 8,
+				name: 'Letterform',
+				values: [
+
+				]
+			},
+			{
+				digit: 9,
+				name: 'Midline',
+				values: [
+
+				]
+			},
+			{
+				digit: 10,
+				name: 'X-height',
+				values: [
+
+				]
+			},
+		]
+	},
+	{
+		digit: 3,
+		name: 'Latin Hand Written',
+		values: [
+			{
+				digit: 0,
+				name: 'Any'
+			},
+			{
+				digit: 1,
+				name: 'No fit'
+			},
+			{
+				digit: 2,
+				name: 'Tool Kind',
+				values: [
+
+				]
+			},
+			{
+				digit: 3,
+				name: 'Weight',
+				values: [
+
+				]
+			},
+			{
+				digit: 4,
+				name: 'Spacing',
+				values: [
+
+				]
+			},
+			{
+				digit: 5,
+				name: 'Aspect Ratio',
+				values: [
+
+				]
+			},
+			{
+				digit: 6,
+				name: 'Contrast',
+				values: [
+
+				]
+			},
+			{
+				digit: 7,
+				name: 'Topology',
+				values: [
+
+				]
+			},
+			{
+				digit: 8,
+				name: 'Form',
+				values: [
+
+				]
+			},
+			{
+				digit: 9,
+				name: 'Finials',
+				values: [
+
+				]
+			},
+			{
+				digit: 10,
+				name: 'X-ascent',
+				values: [
+
+				]
+			},
+		]
+	},
+	{
+		digit: 4,
+		name: 'Latin Decorative',
+		values: [
+			{
+				digit: 0,
+				name: 'Any'
+			},
+			{
+				digit: 1,
+				name: 'No fit'
+			},
+			{
+				digit: 2,
+				name: 'Class',
+				values: [
+
+				]
+			},
+			{
+				digit: 3,
+				name: 'Weight',
+				values: [
+
+				]
+			},
+			{
+				digit: 4,
+				name: 'Aspect',
+				values: [
+
+				]
+			},
+			{
+				digit: 5,
+				name: 'Contrast',
+				values: [
+
+				]
+			},
+			{
+				digit: 6,
+				name: 'Serif Variant',
+				values: [
+
+				]
+			},
+			{
+				digit: 7,
+				name: 'Treatment',
+				values: [
+
+				]
+			},
+			{
+				digit: 8,
+				name: 'Lining',
+				values: [
+
+				]
+			},
+			{
+				digit: 9,
+				name: 'Topology',
+				values: [
+
+				]
+			},
+			{
+				digit: 10,
+				name: 'Range of Characters',
+				values: [
+
+				]
+			},
+		]
+	},
+	{
+		digit: 5,
+		name: 'Latin Symbol',
+		values: [
+			{
+				digit: 0,
+				name: 'Any'
+			},
+			{
+				digit: 1,
+				name: 'No fit'
+			},
+			{
+				digit: 2,
+				name: 'Kind',
+				values: [
+
+				]
+			},
+			{
+				digit: 3,
+				name: 'Weight',
+				values: [
+
+				]
+			},
+			{
+				digit: 4,
+				name: 'Spacing',
+				values: [
+
+				]
+			},
+			{
+				digit: 5,
+				name: 'Aspect Ratio & Contrast',
+				values: [
+
+				]
+			},
+			{
+				digit: 6,
+				name: 'Aspect Ratio of Character 94',
+				values: [
+
+				]
+			},
+			{
+				digit: 7,
+				name: 'Aspect Ratio of Character 119',
+				values: [
+
+				]
+			},
+			{
+				digit: 8,
+				name: 'Aspect Ratio of Character 157',
+				values: [
+
+				]
+			},
+			{
+				digit: 9,
+				name: 'Aspect Ratio of Character 163',
+				values: [
+
+				]
+			},
+			{
+				digit: 10,
+				name: 'Aspect Ratio of Character 211',
+				values: [
+
+				]
+			},
+		]
+	}
+];
