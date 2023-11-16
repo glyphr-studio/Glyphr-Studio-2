3a008c08a3f30d0f5c97241ac6fe9cb2d109bf79
Matt LaGrandeur <mattlag@gmail.com>
2023-11-15 17:00:44 -0800

    Cross Project Actions welcome page :sunflower:

--- src/app/app.js
+++ src/app/app.js
@@ -41,9 +41,9 @@ export class GlyphrStudioApp {
 				// Internal Dev Stuff
 				mode: true, // {bool} global switch for all the stuff below
 				overwriteTitle: true, // {bool} Use a 'Dev Mode' window title
-				sampleProject: false, // {bool or 'oblegg'} Load the sample project
-				twoSampleProjects: false, // {bool} Load two sample projects
-				currentPage: false, // {Sentence case page name} navigate straight to a page
+				sampleProject: 'oblegg', // {bool or 'oblegg'} Load the sample project
+				twoSampleProjects: true, // {bool} Load two sample projects
+				currentPage: 'Characters', // {Sentence case page name} navigate straight to a page
 				currentGlyphID: false, // {glyph id} select a glyph
 				currentPanel: false, // {Title case panel name} navigate straight to a panel
 				currentTool: false, // {Tool name} select a tool
