e11480eed36f7415ecabb28fc57365689ea512b9
Matt LaGrandeur <mattlag@gmail.com>
2024-01-30 17:38:41 -0800

    Working, except for Winding

--- src/project_editor/boolean_combine_new.js
+++ src/project_editor/boolean_combine_new.js
@@ -1,6 +1,11 @@
 // import * as paper from '../lib/paper-core.js';
 // --------------------------------------------------------------
 // Combine all paths
+
+import { getCurrentProjectEditor } from '../app/main';
+import { addPathToCurrentItem } from '../edit_canvas/tools/tools';
+import { ioSVG_convertSVGTagsToGlyph } from '../formats_io/svg_outline_import';
+
 // --------------------------------------------------------------
 const paper = window.paper;
 /**
@@ -8,9 +13,43 @@ const paper = window.paper;
  * @param {Array} paths - paths to combine
  * @returns {Array} - resulting paths
  */
-export function combineAllPaths(paths = []) {
+export function combineAllPaths(gsPaths = []) {
+	log(`combineAllPaths (PAPER)`, 'start');
+	log(`\n⮟gsPaths⮟`);
+	log(gsPaths);
+
 	paper.setup();
-	let paperPath = new paper.Path();
-	console.log(paperPath);
-	console.log(paper.PathItem.unite(paperPath));
+
+	const paperPaths = [];
+
+	gsPaths.forEach((gsPath) => {
+		paperPaths.push(new paper.Path(gsPath.makeSVGPathData()));
+	});
+
+	log(`\n⮟paperPaths⮟`);
+	log(paperPaths);
+
+	let combinedPath = new paper.Path();
+
+	paperPaths.forEach((paperPath) => {
+		combinedPath = combinedPath.unite(paperPath);
+	});
+
+	log(`\n⮟combinedPath⮟`);
+	log(combinedPath);
+	log(combinedPath.exportSVG().getAttribute('d'));
+	log(combinedPath.exportSVG());
+
+	const newPathData = combinedPath.exportSVG().getAttribute('d');
+
+	const newGSPaths = ioSVG_convertSVGTagsToGlyph(
+		`<svg><path d="${newPathData}"></path></svg>`
+	).shapes;
+	log(`\n⮟newGSPaths⮟`);
+	log(newGSPaths);
+	const editor = getCurrentProjectEditor();
+	editor.multiSelect.shapes.deleteShapes();
+	newGSPaths.forEach((newPath) => addPathToCurrentItem(newPath));
+
+	log(`combineAllPaths (PAPER)`, 'end');
 }
