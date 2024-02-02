d7e2e157d2406285df64341f610951ffae203e9a
Matt LaGrandeur <mattlag@gmail.com>
2024-02-01 18:34:55 -0800

    Buttons for all the combine actions

--- src/project_editor/boolean_combine_new.js
+++ src/project_editor/boolean_combine_new.js
@@ -1,55 +1,93 @@
-// import * as paper from '../lib/paper-core.js';
-// --------------------------------------------------------------
-// Combine all paths
-
 import { paper } from '../app/app';
 import { getCurrentProjectEditor } from '../app/main';
+import { clone } from '../common/functions';
 import { addPathToCurrentItem } from '../edit_canvas/tools/tools';
-import { convert_makeGlyphrStudioPath, convert_makePaperPath } from '../paper_conversions';
-
-// --------------------------------------------------------------
+import { ioSVG_convertSVGTagsToGlyph } from '../formats_io/svg_outline_import';
 
 /**
  * Combines an array of paths, separating them out by winding
  * @param {Array} paths - paths to combine
  * @returns {Array} - resulting paths
  */
-export function combineAllPaths(gsPaths = []) {
-	log(`combineAllPaths (PAPER)`, 'start');
+export function combinePaths(gsPaths = [], operation = 'unite') {
+	log(`combinePaths`, 'start');
 	log(`\n⮟gsPaths⮟`);
 	log(gsPaths);
 
+	// Convert to Paper
 	let paperPaths = [];
 
 	gsPaths.forEach((gsPath) => {
-		paperPaths = paperPaths.concat(convert_makePaperPath(gsPath));
+		paperPaths = paperPaths.concat(convertToPaperPath(gsPath));
 	});
 
 	log(`\n⮟paperPaths⮟`);
 	log(paperPaths);
 
+	// Combine
 	let combinedPath = new paper.Path();
 
 	paperPaths.forEach((paperPath) => {
-		combinedPath = combinedPath.unite(paperPath);
+		if(operation === 'unite') combinedPath = combinedPath.unite(paperPath);
+		if(operation === 'divide') combinedPath = combinedPath.divide(paperPath);
+		if(operation === 'subtract') combinedPath = combinedPath.subtract(paperPath);
+		if(operation === 'exclude') combinedPath = combinedPath.exclude(paperPath);
+		if(operation === 'intersect') combinedPath = combinedPath.intersect(paperPath);
 	});
 
-	log(`\n⮟combinedPath⮟`);
-	log(combinedPath);
-	log(combinedPath.exportSVG().getAttribute('d'));
-	log(combinedPath.exportSVG());
-
-	let newGSPaths = [];
-	newGSPaths = convert_makeGlyphrStudioPath(combinedPath);
-
+	// Convert back to Glyphr Studio
+	let newGSPaths = convertToGlyphrStudioPath(combinedPath);
 	log(`\n⮟newGSPaths⮟`);
 	log(newGSPaths);
+
+	// Update the project
 	const editor = getCurrentProjectEditor();
 	editor.multiSelect.shapes.deleteShapes();
 	newGSPaths.forEach((newPath) => addPathToCurrentItem(newPath));
+	// TODO add history
 
-	log(`combineAllPaths (PAPER)`, 'end');
+	log(`combinePaths`, 'end');
 }
+
+// --------------------------------------------------------------
+// Conversion Functions
+// --------------------------------------------------------------
+
+function convertToPaperPath(gsPath) {
+	log(`convertToPaperPath`, 'start');
+	log(`\n⮟gsPath⮟`);
+	log(gsPath);
+
+	const pathData = gsPath.svgPathData;
+	log(`\n⮟pathData⮟`);
+	log(pathData);
+	const resultPath = new paper.Path(pathData);
+
+	log(`\n⮟resultPath⮟`);
+	log(resultPath);
+	log(`convertToPaperPath`, 'end');
+	return resultPath;
+}
+
+function convertToGlyphrStudioPath(paperPath) {
+	log(`convertToGlyphrStudioPath`, 'start');
+
+	const newPathSVG = paperPath.exportSVG();
+	log(newPathSVG);
+	const newGSPaths = ioSVG_convertSVGTagsToGlyph(
+		`<svg><path d="${newPathSVG.getAttribute('d')}"></path></svg>`
+	).shapes;
+
+	log(`\n⮟newGSPaths[0]⮟`);
+	log(clone(newGSPaths[0]));
+
+	log(`convertToGlyphrStudioPath`, 'end');
+	return newGSPaths;
+}
+
+// --------------------------------------------------------------
+// TEST FUNCTIONS
+// --------------------------------------------------------------
 
 export function paperRoundTripTest(gsPaths = []) {
 	log(`paperRoundTripTest`, 'start');
@@ -59,7 +97,7 @@ export function paperRoundTripTest(gsPaths = []) {
 	let newPaperPaths = [];
 
 	gsPaths.forEach((gsPath) => {
-		newPaperPaths = newPaperPaths.concat(convert_makePaperPath(gsPath));
+		newPaperPaths = newPaperPaths.concat(convertToPaperPath(gsPath));
 	});
 
 	log(`\n⮟newPaperPaths⮟`);
@@ -68,7 +106,7 @@ export function paperRoundTripTest(gsPaths = []) {
 	let newGSPaths = [];
 
 	newPaperPaths.forEach((paperPath) => {
-		newGSPaths = newGSPaths.concat(convert_makeGlyphrStudioPath(paperPath));
+		newGSPaths = newGSPaths.concat(convertToGlyphrStudioPath(paperPath));
 	});
 
 	log(`\n⮟newGSPaths⮟`);
