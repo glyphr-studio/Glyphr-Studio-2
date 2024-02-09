3510f57d287e450665738a40264c62018108c27a
Matt LaGrandeur <mattlag@gmail.com>
2024-02-08 16:01:14 -0800

    Getting vitest plugin to work

--- src/project_data/tests/component_instance.test.js
+++ src/project_data/tests/component_instance.test.js
@@ -1,6 +1,5 @@
 import { describe, expect, it } from 'vitest';
 import { ComponentInstance } from '../component_instance.js';
-
 // --------------------------------------------------------------
 // CHECKLIST
 // >> depends on root glyph
