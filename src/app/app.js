5588993793e265cd37e435ba44f757b2fdcd1aa6
Matt LaGrandeur <mattlag@gmail.com>
2024-06-17 16:29:16 -0700

    Feature: differentiation for created vs. not created items

--- src/app/app.js
+++ src/app/app.js
@@ -48,7 +48,7 @@ export class GlyphrStudioApp {
 				overwriteTitle: true, // {bool} Use a 'Dev Mode' window title
 				sampleProject: false, // {true/false, 'oblegg', 'bool'} Load the sample project
 				twoSampleProjects: false, // {bool} Load two sample projects
-				currentPage: false, // {Sentence case page name} navigate straight to a page
+				currentPage: 'Overview', // {Sentence case page name} navigate straight to a page
 				currentGlyphID: false, // {glyph id} select a glyph
 				currentPanel: false, // {Title case panel name} navigate straight to a panel
 				currentTool: false, // {Tool name} select a tool
