0583c1fd7972755c268e28d8207508a394b6b648
Matt LaGrandeur <mattlag@gmail.com>
2024-06-28 12:23:30 -0700

    canvas mock now works

--- vitest.config.js
+++ vitest.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
 	test: {
 		globals: true,
-		environment: 'jsdom'
+		environment: 'jsdom',
+		setupFiles: ['vi-canvas-mock'],
 	},
 });
