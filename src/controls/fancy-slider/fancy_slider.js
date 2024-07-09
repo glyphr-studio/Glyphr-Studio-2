d39bf3db5e9bd384005bb683a55749d4f4828dfb
Matt LaGrandeur <mattlag@gmail.com>
2024-07-09 11:32:23 -0700

    Storybook: Fancy Slider

--- src/controls/fancy-slider/fancy_slider.js
+++ src/controls/fancy-slider/fancy_slider.js
@@ -3,7 +3,7 @@ import { addAsChildren, makeElement } from '../../common/dom.js';
 /**
  * Makes a styled slider element
  * @param {Number} initialValue - what the slider value should be
- * @param {Function} callback - what to do when it changes
+ * @param {Function | false} callback - what to do when it changes
  * @param {Number} min - min value for the slider
  * @param {Number} max - max value for the slider
  * @param {Number} step - how big each step is
@@ -17,12 +17,15 @@ export function makeFancySlider(initialValue = 50, callback, min = 0, max = 100,
 		className: 'fancy-slider__slider-readout',
 		innerHTML: '' + initialValue,
 	});
+
+	const normalizedCurrentValue = (initialValue / (max - min)) * 100;
+
 	let bar = makeElement({
 		tag: 'input',
 		attributes: {
 			type: 'range',
 			value: initialValue,
-			style: `accent-color: hsl(${initialValue + 200}, 100%, 40%);`,
+			style: `accent-color: hsl(${normalizedCurrentValue + 200}, 100%, 40%);`,
 			min: min,
 			max: max,
 			step: step,
@@ -33,7 +36,9 @@ export function makeFancySlider(initialValue = 50, callback, min = 0, max = 100,
 		// @ts-ignore
 		const value = parseInt(event.target.value);
 		sliderReadout.innerHTML = '' + value;
-		bar.setAttribute('style', `accent-color: hsl(${value + 200}, 100%, 40%);`);
+
+		const normalizedCurrentValue = (value / (max - min)) * 100;
+		bar.setAttribute('style', `accent-color: hsl(${normalizedCurrentValue + 200}, 100%, 40%);`);
 		if (callback) callback(value);
 	});
 
