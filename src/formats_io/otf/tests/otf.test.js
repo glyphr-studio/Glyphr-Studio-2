959decc94d4a0ba8dbce377c1e87b143558bed56
Matt LaGrandeur <mattlag@gmail.com>
2024-10-10 09:56:28 -0700

    Additional import tests

--- src/formats_io/otf/tests/otf.test.js
+++ src/formats_io/otf/tests/otf.test.js
@@ -4,8 +4,8 @@ import { describe, expect, it } from 'vitest';
 import openTypeJS from '../../../lib/opentype.js-september-2024-kern-write/opentype.mjs';
 import { ioFont_importFont } from '../font_import.js';
 
-describe('OTF Font', () => {
-	it('Import: Most Basic Test', async () => {
+describe('Most Basic Test Fonts', () => {
+	it('Import: OTF Most Basic Test', async () => {
 		const mostBasicTestOTF = './MostBasicTestRegular.otf';
 		const filePath = path.resolve(__dirname, mostBasicTestOTF);
 		const fontBuffer = fs.readFileSync(filePath);
@@ -23,7 +23,45 @@ describe('OTF Font', () => {
 		// Metadata
 		expect(result.settings.font.style).toEqual('Regular');
 	});
+	it('Import: TTF Most Basic Test', async () => {
+		const mostBasicTestTTF = './MostBasicTest.ttf';
+		const filePath = path.resolve(__dirname, mostBasicTestTTF);
+		const fontBuffer = fs.readFileSync(filePath);
+		const loadResult = openTypeJS.parse(fontBuffer);
+		const result = await ioFont_importFont(loadResult, true);
+		expect(result).toBeTruthy();
 
+		// Glyphs
+		const capT = result.glyphs['glyph-0x54'];
+		expect(capT).toBeTruthy();
+		expect(capT.advanceWidth).toEqual(1000);
+		expect(capT.shapes.length).toEqual(2);
+		expect(capT.shapes[0].pathPoints.length).toEqual(4);
+
+		// Metadata
+		expect(result.settings.font.style).toEqual('Regular');
+	});
+	it('Import: WOFF Most Basic Test', async () => {
+		const mostBasicTestWOFF = './MostBasicTest.woff';
+		const filePath = path.resolve(__dirname, mostBasicTestWOFF);
+		const fontBuffer = fs.readFileSync(filePath);
+		const loadResult = openTypeJS.parse(fontBuffer);
+		const result = await ioFont_importFont(loadResult, true);
+		expect(result).toBeTruthy();
+
+		// Glyphs
+		const capT = result.glyphs['glyph-0x54'];
+		expect(capT).toBeTruthy();
+		expect(capT.advanceWidth).toEqual(1000);
+		expect(capT.shapes.length).toEqual(2);
+		expect(capT.shapes[0].pathPoints.length).toEqual(4);
+
+		// Metadata
+		expect(result.settings.font.style).toEqual('Regular');
+	});
+});
+
+describe('OTF Extended Test Font', () => {
 	it('Import: Oblegg Extended Test', async () => {
 		const obleggExtendedTestOTF = './ObleggExtendedTestRegular.otf';
 		const filePath = path.resolve(__dirname, obleggExtendedTestOTF);
