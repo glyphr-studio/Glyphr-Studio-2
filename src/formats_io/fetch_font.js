ec2f522b888762ae00117da2892db35879cf9999
Oscar Moreno <oscar.moreno@enviaflores.com>
2024-12-27 13:02:32 -0600

    chore: remove unnessary comments

--- src/formats_io/fetch_font.js
+++ src/formats_io/fetch_font.js
@@ -6,8 +6,7 @@ import { enFontUrl } from '../utils/env/envFontUrl';
  * @param {string} id - font id
  */
 export async function fetchFont(id) {
-    const response = await fetch(' https://ep-product-pictures.s3.us-west-2.amazonaws.com/fonts/99f8e8ca-6339-4493-a9df-b515b1fee48b.ttf');
-    // const response = await fetch(`${window.location.origin}/${enFontUrl()}/${id}`);
+    const response = await fetch(`${window.location.origin}/${enFontUrl()}/${id}`);
     const fontData = await response.arrayBuffer();
     const font = await openTypeJS.parse(fontData);
     return font
