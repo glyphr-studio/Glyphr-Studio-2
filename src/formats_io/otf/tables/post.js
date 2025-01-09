aa08cc7de59c3c12c4ce886cb82b2bf4753d39e8
Matt LaGrandeur <mattlag@gmail.com>
2025-01-09 14:39:25 -0800

    Bug: Reading ItalicAngle from OTF

---
+++ src/formats_io/otf/tables/post.js
@@ -0,0 +1,12 @@
+import { GlyphrStudioProject } from '../../../project_data/glyphr_studio_project';
+
+/**
+ * Finds metadata from the post table in the opentype.js font object,
+ * and pulls appropriate data into a provided Glyphr Studio Project.
+ * @param {Object} importedFont - opentype.js font object
+ * @param {GlyphrStudioProject} project - current Glyphr Studio Project
+ */
+export function importTable_post(importedFont, project) {
+	const fontSettings = project.settings.font;
+	fontSettings.italicAngle = importedFont.tables?.post?.italicAngle || 0;
+}
