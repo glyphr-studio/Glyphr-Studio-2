e4936aad4bcec66d505944553492ff6b843cc0b0
Matt LaGrandeur <mattlag@gmail.com>
2025-01-27 10:41:48 -0800

    New Feature: Auto Side Bearings on SVG Import

--- src/app/cross_project_actions/action_overwrite_settings.js
+++ src/app/cross_project_actions/action_overwrite_settings.js
@@ -104,6 +104,7 @@ function makeRows(parent) {
 	makeOneRow('app', 'exportLigatures');
 	makeOneRow('app', 'exportKerning');
 	makeOneRow('app', 'moveShapesOnSVGDragDrop');
+	makeOneRow('app', 'autoSideBearingsOnSVGDragDrop');
 	makeOneRow('font', 'family');
 	makeOneRow('font', 'style');
 	makeOneRow('font', 'version');
