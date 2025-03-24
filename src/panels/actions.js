a4a13714fa6a25b7f01c95f73cdb87c79f8e09ee
Matt LaGrandeur <mattlag@gmail.com>
2025-03-24 16:41:45 -0700

    Stubbing in Transforms action button

--- src/panels/actions.js
+++ src/panels/actions.js
@@ -181,6 +181,17 @@ export function getActionData(name) {
 				},
 			},
 			{
+				iconName: 'transforms',
+				title: 'Transform\nScale, rotate, or skew the currently selected shape(s).',
+				onClick: () => {
+					// const editor = getCurrentProjectEditor();
+					// let shape = editor.multiSelect.shapes.virtualGlyph;
+					// shape.flipNS();
+					// editor.history.addState(`Flipped shape ${shape.name} vertically`);
+					// editor.publish('currentItem', editor.selectedItem);
+				},
+			},
+			{
 				iconName: 'round',
 				title: `Round all path point and handle position values\nIf a x or y value for any point or a handle in the path has decimals, it will be rounded to the nearest whole number.`,
 				onClick: () => {
@@ -351,6 +362,17 @@ export function getActionData(name) {
 					shape.flipNS();
 					editor.history.addState(`Flipped shape ${shape.name} vertically`);
 					editor.publish('currentItem', editor.selectedItem);
+				},
+			},
+			{
+				iconName: 'transforms',
+				title: 'Transform\nScale, rotate, or skew the currently selected shape(s).',
+				onClick: () => {
+					// const editor = getCurrentProjectEditor();
+					// let shape = editor.multiSelect.shapes.virtualGlyph;
+					// shape.flipNS();
+					// editor.history.addState(`Flipped shape ${shape.name} vertically`);
+					// editor.publish('currentItem', editor.selectedItem);
 				},
 			},
 			{
