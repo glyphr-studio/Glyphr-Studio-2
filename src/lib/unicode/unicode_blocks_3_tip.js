81f917ebc51485c1579e5a8a6c3de04620c7bdef
Matt LaGrandeur <mattlag@gmail.com>
2025-07-27 22:15:26 -0700

    Bug: Importing chars to unallocated ranges

--- src/lib/unicode/unicode_blocks_3_tip.js
+++ src/lib/unicode/unicode_blocks_3_tip.js
@@ -2,3 +2,7 @@ export const unicodeBlocksTIP = [
 	{ begin: 0x30000, end: 0x3134f, name: 'CJK Unified Ideographs Extension G' },
 	{ begin: 0x31350, end: 0x323af, name: 'CJK Unified Ideographs Extension H' },
 ];
+
+export const unicodeBlocksTIPUnallocated = [
+	{ begin: 0x323b0, end: 0x3ffff, name: 'TIP: Unallocated Block 1' },
+];
