0280957ff2935ec36d2594dfd0af6c772eebb73e
Matt LaGrandeur <mattlag@gmail.com>
2026-02-04 13:36:36 -0800

    Storybook update

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,7 +2,7 @@
 const config = {
 	// stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
 	stories: ['../src/controls/**/*.stories.js'],
-	addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@chromatic-com/storybook'],
+	addons: ['@storybook/addon-links', '@chromatic-com/storybook'],
 	framework: {
 		name: '@storybook/web-components-vite',
 		options: {},
