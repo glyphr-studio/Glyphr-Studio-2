4d309e0380fd4bacc4c3b011bd3bd87e03109361
Matt LaGrandeur <mattlag@gmail.com>
2026-07-12 13:27:00 -0700

    Font preview fix test

--- dist/assets/__vite-browser-external-Bj4RUjne.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./index-DzfJaFwI.js";var t=e(((e,t)=>{t.exports={}}));export default t();
---
+++ dist/assets/__vite-browser-external-eqEDXIeF.js
@@ -0,0 +1,1 @@
+import{t as e}from"./index-C6FwdP3t.js";var t=e(((e,t)=>{t.exports={}}));export default t();
